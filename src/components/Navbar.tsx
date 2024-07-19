794fa1d3eeed69fe0c711804b50fe195816610a8
ceid1987 <c.eid.1312@gmail.com>
2024-07-19 19:38:07 +0200

    Fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,20 +9,21 @@ const Navbar: React.FC = () => {
   const pathname = usePathname();
   const links = [
     { href: '/', label: 'home' },
-    { href: '/projects', label: 'projects' },
+    { href: '/about', label: 'about' },
+    { href: '/projects', label: 'projects' },    
     { href: '/contact', label: 'contact' }
   ];
 
   return (
-    <nav className="bg-black rounded-full px-12 py-4 flex justify-center space-x-10 mt-4 mx-auto max-w-screen-lg">
+    <nav className="bg-black rounded-full px-6 md:px-16 space-x-8 md:space-x-16 py-4 flex justify-center mt-6 mx-auto">
       {links.map((link) => (
         <Link key={link.href} href={link.href} className="relative text-white hover:text-purple-500">
           <span
-            className={`relative text-white hover:text-purple-500 ${
+            className={`relative text-white hover:text-purple-500  ${
               pathname === link.href ? 'font-bold' : ''
             }`}
           >
-            {pathname === link.href && <span className="absolute left-[-16px] text-purple-500">{`> `}</span>}
+            {pathname === link.href && <span className="absolute left-[-10px] text-purple-500">{`> `}</span>}
             {link.label}
           </span>
         </Link>
