4084e762fb578e748dd830818a2384b50ff8b8c9
ceid1987 <c.eid.1312@gmail.com>
2024-07-22 21:08:20 +0200

    Add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+// Dependencies
 import React, { useEffect, useState } from 'react';
+import { faExternalLink, faExternalLinkSquare, faEye } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Components
 import Navbar from '../components/Navbar';
 import AnimatedText from '../components/AnimatedText';
 import AnimatedArrow from '../components/AnimatedArrow';
+
+// Styles
 import styles from '../styles/Parallax.module.css';
+import AsciiBackground from '@/components/AsciiBackground';
+
 
 const Home: React.FC = () => {
   const [currentSection, setCurrentSection] = useState('home');
@@ -13,7 +22,7 @@ const Home: React.FC = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const sections = ['home', 'about', 'projects', 'contact'];
+      const sections = ['home', 'whoami', 'projects', 'contact'];
       const offsets = sections.map(
         section => document.getElementById(section)?.offsetTop || 0
       );
@@ -51,13 +60,43 @@ const Home: React.FC = () => {
         <div className={`${styles.parallax} ${styles['parallax-fast']}`}>
           <AnimatedText />
         </div>
-        <h2 className={`${styles.parallax} ${styles['parallax-fast']} text-l mt-4 md:text-xl lg:text-2xl transition-opacity duration-1000 ${scrollY > 200 ? 'opacity-0' : 'opacity-100'}`}>
+        <h2 className={`${styles.parallax} ${styles['parallax-fast']} text-l mt-4 md:text-xl lg:text-2xl transition-opacity duration-1000 ${scrollY > 300 ? 'opacity-0' : 'opacity-100'}`}>
           software / devops engineer
         </h2>
         <AnimatedArrow visible={showArrow} />
       </div>
-      <div id="about" className="min-h-screen flex flex-grow flex-col items-center justify-center">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl">About Section</h1>
+      <div id="whoami" className="min-h-screen flex flex-col items-center justify-center bg-black text-white  p-12">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl mt-18 py-16">whoami</h1>
+        <AsciiBackground />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 w-full max-w-6xl p-16">
+          <div>
+            <h2 className="text-xl md:text-2xl lg:text-3xl text-purple-500 mb-4">about me</h2>
+            <p className="text-lg md:text-2lg lg:text-xl">
+              Software/DevOps engineer from 🇱🇧 Lebanon, based in 🇫🇷 France. I like to develop scalable solutions from the ground up, all the way to deployment :)
+            </p>
+          </div>
+          <div>
+            <h2 className="text-xl md:text-2xl lg:text-3xl text-purple-500 mb-4">tech stack</h2>
+            <div className="flex items-center justify-center h-32 bg-gray-700 rounded-lg">
+              {/* Add your tech stack slider here */}
+              <p>Tech stack slider placeholder</p>
+            </div>
+          </div>
+          <div>
+            <h2 className="text-xl md:text-2xl lg:text-3xl text-purple-500 mb-4">experience</h2>
+            <button className="bg-white bg-opacity-10 backdrop-blur-lg text-white py-2 px-4 rounded-full text-lg mt-2 ">
+              view timeline<FontAwesomeIcon icon={faEye} className="ml-2 fa-xs"/> 
+            </button>
+            <a href="#" className="block mt-4 text-white underline ">full resume<FontAwesomeIcon icon={faExternalLink} className="ml-2 fa-xs"/></a>
+          </div>
+          <div>
+            <h2 className="text-xl md:text-2xl lg:text-3xl text-purple-500 mb-4">now playing</h2>
+            <div className="flex items-center justify-center h-32 bg-gray-700 rounded-lg">
+              {/* Add your now playing content here */}
+              <p>Now playing placeholder</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div id="projects" className="min-h-screen flex flex-grow flex-col items-center justify-center">
         <h1 className="text-3xl md:text-4xl lg:text-5xl">Projects Section</h1>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,16 +9,16 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ currentSection }) => {
   const links = [
     { href: '#home', label: 'home' },
-    { href: '#about', label: 'about' },
+    { href: '#whoami', label: 'whoami' },
     { href: '#projects', label: 'projects' },
     { href: '#contact', label: 'contact' }
   ];
 
   return (
-    <nav className="fixed mt-14 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black rounded-full px-6 md:px-16 space-x-8 md:space-x-18 py-4 flex justify-center mx-auto z-50">
+    <nav className="fixed mt-14 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-10 backdrop-blur-lg rounded-full px-6 md:px-16 space-x-8 md:space-x-18 py-4 flex justify-center mx-auto z-50 ">
       {links.map(link => (
         <Link key={link.href} href={link.href} className="relative text-white hover:text-purple-500">
-          <span className={`relative text-white hover:text-purple-500 ${currentSection === link.href.slice(1) ? 'font-bold' : ''}`}>
+          <span className={`relative ${currentSection === link.href.slice(1) ? 'font-bold' : ''}`}>
             {currentSection === link.href.slice(1) && <span className="absolute left-[-10px] text-purple-500">{`>`}</span>}
             {link.label}
           </span>
